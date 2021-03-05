f0458a242e66fa5d62bf52adc9fa67e73ffeaf82
twoSad <dv.medinskiy@gmail.com>
2021-03-05 15:26:46 +0200

    add header links

--- src/Components/CV/CV.jsx
+++ src/Components/CV/CV.jsx
@@ -293,7 +293,9 @@ const CV = (props) => {
                         <ul>
                             <li>
                                 <div className="info">
-                                    <p className="semi-bold">Random Quotes</p>
+                                    <a className='header_link' href="/RandomQuote" rel="noreferrer" target="_self">
+                                        <p className="semi-bold">Random Quotes</p>
+                                    </a>
                                     <p className='text'>This project gives you a random quote from a famous person. If you want, you can share the quote on Twitter or on Tumblr</p>
                                     <p>
                                         <span className='hashtag'>#React</span> <span className='hashtag'>#Redux</span> <span className='hashtag'>#RestAPI</span> <span className='hashtag'>#ReactBootstrap</span> <span className='hashtag'>#JS</span> <span className='hashtag'>#HTML</span> <span className='hashtag'>#CSS</span>
@@ -303,7 +305,9 @@ const CV = (props) => {
                             <hr />
                             <li>
                                 <div className="info">
-                                    <p className="semi-bold">Markdown Previewer</p>
+                                    <a className='header_link' href="/MarkdownPreviewer" rel="noreferrer" target="_self">
+                                        <p className="semi-bold">Markdown Previewer</p>
+                                    </a>
                                     <p className='text'>This is the simple markdown editor with live preview that includes a markup syntax guide and laconic design</p>
                                     <p>
                                         <span className='hashtag'>#React</span> <span className='hashtag'>#HTML</span> <span className='hashtag'>#SCSS</span> <span className='hashtag'>#MarkedLibrary</span>
@@ -313,7 +317,9 @@ const CV = (props) => {
                             <hr />
                             <li>
                                 <div className="info">
-                                    <p className="semi-bold">Drum Machine</p>
+                                    <a className='header_link' href="/DrumMachine" rel="noreferrer" target="_self">
+                                        <p className="semi-bold">Drum Machine</p>
+                                    </a>
                                     <p className='text'>Drum pad machine with 2 sound presets, volume control, on/off button and display which indication current action. Also funny animation (on high resolution monitor)</p>
                                     <p>
                                         <span className='hashtag'>#React</span> <span className='hashtag'>#ReactBootstrap</span> <span className='hashtag'>#JS</span> <span className='hashtag'>#HTML</span> <span className='hashtag'>#SCSS</span> <span className='hashtag'>#Audio</span>
@@ -323,7 +329,9 @@ const CV = (props) => {
                             <hr />
                             <li>
                                 <div className="info">
-                                    <p className="semi-bold">Calculator</p>
+                                    <a className='header_link' href="/Calculator" rel="noreferrer" target="_self">
+                                        <p className="semi-bold">Calculator</p>
+                                    </a>
                                     <p className='text'>Calculator with common functionality (adding, subtraction, multiplication, division). Also correct work with negative values</p>
                                     <p>
                                         <span className='hashtag'>#React</span> <span className='hashtag'>#JS</span> <span className='hashtag'>#Regex</span> <span className='hashtag'>#HTML</span> <span className='hashtag'>#CSS</span>
@@ -333,7 +341,9 @@ const CV = (props) => {
                             <hr />
                             <li>
                                 <div className="info">
-                                    <p className="semi-bold">Tomato Timer</p>
+                                    <a className='header_link' href="/TomatoTimer" rel="noreferrer" target="_self">
+                                        <p className="semi-bold">Tomato Timer</p>
+                                    </a>
                                     <p className='text'>Flexible and easy to use Tomato Timer where user can configure work session and break time interval, for greater convenience and opportunities to adapt to different tasks. Session and break combination in infinite loop. At the end of any period user will hear a signal to end the current session </p>
                                     <p>
                                         <span className='hashtag'>#React</span> <span className='hashtag'>#JS</span> <span className='hashtag'>#Audio</span> <span className='hashtag'>#TimeManager</span> <span className='hashtag'>#HTML</span> <span className='hashtag'>#CSS</span>
